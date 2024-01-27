6d40b13e7d37ce14688114d5bd6d0f283910b787
Blerta Jusufii <blertamicrosoft10@gmail.com>
2024-01-27 20:12:43 +0100

    jobposts and jobdetails DONE

--- app/views/job/job_list.php
+++ app/views/job/job_list.php
@@ -14,26 +14,28 @@
 
         <div class="listnav">
             <h2>JobPosts</h2>
-            <button>Create a JobPost</button>
+          
         </div>
 
         <div class="jobs">
             <div class="job">
                 <div class="job-left">
                     <div>
-                        <img src="../../../images/buss.png" alt="">
+                        <img src="../../../images/bd.png" alt="">
                     </div>
                     <div>
-                        <h4>Bussines Development</h4>
+                        <h4>Bussines Developer</h4>
                         <p>Madrid, Spain</p>
                     </div>
                 </div>
 
-                <div class="job-right">
+                <div class="job-middle">
                     <h3>7 days left</h3>
                     <p>to receive candidates</p>
                 </div>
-
+                <div class="job-right">
+                <a href="./apply_job.php?jobpost_id=1">Apply for this job</a>
+                </div>
 
             </div>
             <div class="job">
@@ -42,22 +44,24 @@
                         <img src="../../../images/web3.png" alt="">
                     </div>
                     <div>
-                        <h4>Web Development</h4>
+                        <h4>Web Developer</h4>
                         <p>London, United Kingdom</p>
                     </div>
                 </div>
 
-                <div class="job-right">
+                <div class="job-middle">
                     <h3>5 days left</h3>
                     <p>to receive candidates</p>
                 </div>
 
-
+                <div class="job-right">
+                    <a href="./apply_job.php?jobpost_id=2">Apply for this job</a>
+                </div>
             </div>
             <div class="job">
                 <div class="job-left">
                     <div>
-                        <img src="../../../images/uiux.png" alt="">
+                        <img src="../../../images/uiux3.png" alt="">
                     </div>
                     <div>
                         <h4>UI/UX Deigner</h4>
@@ -65,64 +69,74 @@
                     </div>
                 </div>
 
-                <div class="job-right">
+                <div class="job-middle">
                     <h3>8 days left</h3>
                     <p>to receive candidates</p>
                 </div>
+                <div class="job-right">
+                    <a href="./apply_job.php?jobpost_id=3">Apply for this job</a>
+                </div>
+
+            </div>
+            <div class="job">
+                <div class="job-left">
+                    <div>
+                        <img src="../../../images/sq.png" alt="">
+                    </div>
+                    <div>
+                        <h4>Software Engineer</h4>
+                        <p>Berlin, Germany</p>
+                    </div>
+                </div>
+
+                <div class="job-middle">
+                    <h3>3 days left</h3>
+                    <p>to receive candidates</p>
+                </div>
+
+                <div class="job-right">
+                    <a href="./apply_job.php?jobpost_id=4">Apply for this job</a>
+                </div>
+            </div>
+            <div class="job">
+                <div class="job-left">
+                    <div>
+                        <img src="../../../images/gd.jpg" alt="">
+                    </div>
+                    <div>
+                        <h4>Graphic Designer</h4>
+                        <p>Madrid, Spain</p>
+                    </div>
+                </div>
+
+                <div class="job-middle">
+                    <h3>14 days left</h3>
+                    <p>to receive candidates</p>
+                </div>
+                <div class="job-right">
+                    <a href="./apply_job.php?jobpost_id=5">Apply for this job</a>
+                </div>
 
 
             </div>
             <div class="job">
                 <div class="job-left">
                     <div>
-                        <img src="../../../images/buss.png" alt="">
+                        <img src="../../../images/fd2.jpg" alt="">
                     </div>
                     <div>
-                        <h4>Bussines Development</h4>
-                        <p>Madrid, Spain</p>
+                        <h4>Front-End Developer</h4>
+                        <p>Prishtina, Kosova</p>
                     </div>
                 </div>
 
-                <div class="job-right">
+                <div class="job-middle">
                     <h3>7 days left</h3>
                     <p>to receive candidates</p>
                 </div>
-
-
-            </div>
-            <div class="job">
-                <div class="job-left">
-                    <div>
-                        <img src="../../../images/buss.png" alt="">
-                    </div>
-                    <div>
-                        <h4>Bussines Development</h4>
-                        <p>Madrid, Spain</p>
-                    </div>
-                </div>
-
                 <div class="job-right">
-                    <h3>7 days left</h3>
-                    <p>to receive candidates</p>
-                </div>
-
-
-            </div>
-            <div class="job">
-                <div class="job-left">
-                    <div>
-                        <img src="../../../images/buss.png" alt="">
-                    </div>
-                    <div>
-                        <h4>Bussines Development</h4>
-                        <p>Madrid, Spain</p>
-                    </div>
-                </div>
-
-                <div class="job-right">
-                    <h3>7 days left</h3>
-                    <p>to receive candidates</p>
-                </div>
+    <a href="./apply_job.php?jobpost_id=6">Apply for this job</a>
+</div>
 
 
             </div>
